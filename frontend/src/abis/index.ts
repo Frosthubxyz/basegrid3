643be1e21af6c9f674d68a3d99fe68a313af7ae7
Iszyeffiong <israel.iszy2336@gmail.com>
2026-06-16 09:43:21 +0100

    Task 36: Generate or export contract ABIs for the frontend

---
+++ frontend/src/abis/index.ts
@@ -0,0 +1,7 @@
+import BaseGridEscrow from "./BaseGridEscrow.json";
+import BaseGridReputation from "./BaseGridReputation.json";
+import BaseGridTaskManager from "./BaseGridTaskManager.json";
+
+export const EscrowABI = BaseGridEscrow.abi;
+export const ReputationABI = BaseGridReputation.abi;
+export const TaskManagerABI = BaseGridTaskManager.abi;
